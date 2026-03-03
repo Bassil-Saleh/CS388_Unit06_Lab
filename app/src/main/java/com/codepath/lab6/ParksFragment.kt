2b022f9e23777049f340e63cacd85d9e54db9a5d
Bassil Saleh <salehbassil42@gmail.com>
2026-03-03 18:40:10 -0500

    Add code for handling API call

--- app/src/main/java/com/codepath/lab6/ParksFragment.kt
+++ app/src/main/java/com/codepath/lab6/ParksFragment.kt
@@ -1,12 +1,17 @@
 package com.codepath.lab6
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
 
 private const val TAG = "ParksFragment"
 private const val API_KEY = BuildConfig.API_KEY
@@ -20,6 +25,45 @@ class ParksFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fetchParks()
+    }
+
+    private fun fetchParks() {
+        val client = AsyncHttpClient()
+        client.get(PARKS_URL, object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers?,
+                json: JSON?
+            ) {
+                Log.i(TAG, "Successfully fetched parks: $json")
+                try {
+                    val parsedJson = createJson().decodeFromString(
+                        ParksResponse.serializer(),
+                        json?.jsonObject.toString()
+                    )
+                    parsedJson.data?.let { list ->
+                        parks.addAll(list)
+                        parksAdapter.notifyDataSetChanged()
+                    }
+                } catch (e: JSONException) {
+                    Log.e(TAG, "Exception: $e")
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "Failed to fetch parks: $statusCode")
+            }
+        })
     }
 
     override fun onCreateView(
